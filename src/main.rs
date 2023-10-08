c1805f434b80af8233d9ce4386207da818ff360a
Jakob Johnson <jakob.johnson.dev@gmail.com>
2023-10-08 22:56:42 +1100

    chapter 3 complete

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,12 @@
-use std::io::{self, stdout};
-use termion::{raw::IntoRawMode, event::Key, input::TermRead};
+#![warn(clippy::all, clippy::pedantic)]
 
-fn die(e: std::io::Error) {
-    panic!("{}", e);
-}
+mod editor;
+mod terminal;
+
+use editor::Editor;
+pub use editor::Position;
+pub use terminal::Terminal;
 
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
-
-    for key in io::stdin().keys() {
-        match key {
-            Ok(key) => match key {
-                Key::Char(c) => {
-                    if c.is_control() {
-                        println!("{:?} \r", c as u8);
-                    } else {
-                        println!("{:?} ({}) \r", c as u8, c);
-                    }
-                }
-                Key::Ctrl('q') => break,
-                _ => println!("{:?} \r", key),
-            },
-            Err(err) => die(err),
-        }
-    }
+    Editor::default().run();
 }
