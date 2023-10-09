dd95c945dc9ceb2ba63b7e575d7a93f5718d1abc
Jakob Johnson <jakob.johnson.dev@gmail.com>
2023-10-09 18:35:00 +1100

    chapter 4 complete

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,16 @@
 
 mod editor;
 mod terminal;
+mod document;
+mod row;
 
+pub use document::Document;
 use editor::Editor;
 pub use editor::Position;
+pub use row::Row;
 pub use terminal::Terminal;
+
+
 
 fn main() {
     Editor::default().run();
